bf763f4db98fb3e4439a96ad8a791dccdd783d38
asarvazyan <areg.sarvazyan@gmail.com>
2019-06-25 22:27:34 +0200

    Adding crossing over.

--- src/evolution.js
+++ src/evolution.js
@@ -18,13 +18,84 @@ function beginEvolution(popSize, rows, cols) {
 	}		
 }
 
+// Returns a random integer given a maximum value
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Creates an Individual 
+function Individual(rows, cols) {
+	this.genes = [];
+	this.numRows = rows;
+	this.numCols = cols;
+
+	// Creates initial random genes
+	for (var i = 0; i < this.numRows; i++) {
+		this.genes.push(new Array(this.numCols).fill(0));
+
+		for (var j = 0; j < this.numCols; j++) {
+			if (Math.random() > 0.5) {
+				this.genes[i][j] = 1;
+			}
+		}
+	}
+
+	// Returns a new individual with the genes of 'this'
+	// crossed over with the ones from 'other'.
+	this.crossover = function(other) {
+		var child = new Individual();
+
+		// Method: 
+		// For each row of the genes matrix, we take a random
+		// part from the genes of 'this' and the rest from the 
+		// genes of 'other' to construct the genes matrix of
+		// the child.
+		// TODO: add option to start new row with 'this' or with
+		// 'other' (now it always begins with the genes of 'this').
+		for (var i = 0; i < child.numRows; i++) {
+			var chooser = getRandomInt(child.numCols);
+			var newRow = [];
+
+			// Construct new row based on random number.
+			// Add first 'chooser' elements of 'this'.
+			newRow.push(this.genes.slice(0, chooser)); 
+			// Add 'numCols - chooser' elements of 'other'.
+			newRow.push(other.genes(chooser));
+
+			child.genes[i] = newRow;
+		}
+
+		return child;
+	};
+
+
+	// Returns the fitness of this individual
+	this.evaluate = function() {
+		var fitness = 0;
+
+		for (var row = 0; row < this.numRows; row++) {
+			for (var col = 0; col < this.numCols; col++ ) {
+				// Fitness will be calculated as:
+				// +1 if an element of the genes of the individual
+				// is the same as the cell of the user's doodle
+				// and +0 otherwise.
+				if (this.genes[row][col] === doodle[row][col]) {
+					fitness++;
+				}
+			}
+		}
+
+		return fitness;
+	};
+}
+
 function Population(popSize, rows, cols) {
 	this.popSize = popSize;
 	this.individuals = [];
 
 	// Creates a random Population of size 'popSize'
 	for (var i = 0; i < this.popSize; i++) {
-		// Each population member's DNA is going to be a
+		// Each population member's genes are going to be a
 		// 2 dimensional array of size (numRows x numCols)
 		// where each element is either a 1 or 0.
 		this.individuals.push(new Individual(rows, cols));	
@@ -34,7 +105,7 @@ function Population(popSize, rows, cols) {
 	// where each element is the fitness of the respective
 	// index's individual and the sum of fitnesses
 	// TODO: take into account the number of white and black cells in doodle
-	// and how many of each are contained in each individual's DNA.
+	// and how many of each are contained in each individual's genes.
 	this.evaluate = function() {
 		var fitnessArray = [];
 		var fitnessSum = 0;
@@ -49,10 +120,27 @@ function Population(popSize, rows, cols) {
 		return [fitnessArray,fitnessSum];
 	};
 
+	// Carries out the operation of evolution (selection of parents,
+	// crossing-over to get child, and child mutation), updating the
+	// population based on the current one.
+	this.evolve = function(fitness, sum) {
+		var newPop = [];
+		for (var i = 0; i < this.popSize; i++) {
+			// Selection of two individuals within the mating pool
+			var parentA = this.select(fitness, sum);
+			var parentB = this.select(fitness, sum);
+
+			// TODO: make sure parentA !== parentB
+			// Get child by crossing-over the genes of the selected individuals
+			var child = parentA.crossover(parentB);
+
+			// Mutation of genes of the child resulting from crossing-over. 
+		}
+	};
+
 	// Returns an individual given a fitness array, where each element is 
 	// the fitness value of the respective index's individual in the 
 	// population array, and the sum of the values in the array.
-
 	this.select = function(fitness, sum) {
 		// Get a random number from 0 to sum and subtract
 		// fitness values from it until it becomes
@@ -60,7 +148,7 @@ function Population(popSize, rows, cols) {
 		// The individual associated with the last subtracted
 		// fitness score will be the one selected.
 		// Method: https://github.com/CodingTrain/Rainbow-Topics/issues/146
-		var chooser = random(sum);
+		var chooser = getRandomInt(sum);
 
 		for (var i = 0; chooser > 0; i++) {
 			chooser -= fitness[i];
@@ -71,55 +159,5 @@ function Population(popSize, rows, cols) {
 		return this.individuals[i];
 	};
 
-	this.evolve = function(fitness, sum) {
-		var newPop = [];
-		for (var i = 0; i < this.popSize; i++) {
-			// Selection of two individuals within the mating pool
-			var parentA = this.select(fitness, sum);
-			var parentB = this.select(fitness, sum);
-
-			// TODO: make sure parentA !== parentB
-			// Crossing-over of DNA of selected individuals
-
-
-			// Mutation of DNA of the child resulting from crossing-over. 
-		}
-	}
-}
-
-// Creates an Individual 
-function Individual(rows, cols) {
-	this.DNA = [];
-	this.numRows = rows;
-	this.numCols = cols;
-
-	// Creates initial random DNA
-	for (var i = 0; i < this.numRows; i++) {
-		this.DNA.push(new Array(this.numCols).fill(0));
-
-		for (var j = 0; j < this.numCols; j++) {
-			if (Math.random(1) > 0.5) {
-				this.DNA[i][j] = 1;
-			}
-		}
-	}
-
-	// Returns the fitness of this individual
-	this.evaluate = function() {
-		var fitness = 0;
-
-		for (var row = 0; row < this.numRows; row++) {
-			for (var col = 0; col < this.numCols; col++ ) {
-				// Fitness will be calculated as:
-				// +1 if an element of the DNA of the individual
-				// is the same as the cell of the user's doodle
-				// and +0 otherwise.
-				if (this.DNA[row][col] === doodle[row][col]) {
-					fitness++;
-				}
-			}
-		}
-
-		return fitness;
-	};
+	
 }
